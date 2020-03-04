c05c17cdceb31e878a03cfc2f93b37961c2a0469
didiermauricio <didiermauricio@gmail.com>
2020-03-04 09:05:04 +0100

    Update AssemblyInfo.cs
    Commit a comment for testing

--- WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//Tsting commits in gitHub - TO Delete
+// Testing commits in gitHub - TO Delete
 [assembly: AssemblyTitle("WindowsFormsApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
